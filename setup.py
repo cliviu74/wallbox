f08e25b29d31c7221439da9ed06a7eb5b62d89e8
Liviu Chiribuca <Liviu Chiribuca>
2020-08-03 17:36:19 +0100

    Updated module version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(name):
 
 setup(
     name='wallbox',
-    version='0.1',
+    version='0.3',
     description="Module for interacting with Wallbox EV charger api",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
